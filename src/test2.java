e0da5e344e3875b3541f7079b9325d5c274ef2d4
luojq <2922622120@qq.com>
2020-07-20 16:20:11 +0800

    idea测试Git上传

---
+++ src/test2.java
@@ -0,0 +1,9 @@
+/**
+ * @author : luojq
+ * @Project: Helloword
+ * @Package PACKAGE_NAME
+ * @Description: TODO
+ * @date 2020年07月20日 13:45
+ */
+public interface test2 {
+}
---
+++ src/tets.java
@@ -0,0 +1,12 @@
+/**
+ * bianque.com
+ * Copyright (C) 2013-2020 All Rights Reserved.
+ */
+
+/**
+ * @author luojq
+ * @version tets.java, v 0.1 2020-07-20 13:44 
+ */
+
+public class tets {
+}
